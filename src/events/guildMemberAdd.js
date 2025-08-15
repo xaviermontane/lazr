08579663f4d80eb4cf97b2f985b33e37fd3fd27b
xaviermontane <xaviermontane3@gmail.com>
2025-08-15 19:37:27 +0200

    fix(import): fix incorrect module imports

--- src/events/guildMemberAdd.js
+++ src/events/guildMemberAdd.js
@@ -1,5 +1,5 @@
 const { Events, EmbedBuilder } = require('discord.js');
-const config = require('../config.json');
+const config = require('../../config.json')
 
 module.exports = {
     name: Events.GuildMemberAdd,
